ed19f823504645c15a18811041ea35d4823b8d6d
Az777 <basaqr001@knights.gannon.edu>
2014-11-24 10:37:00 -0500

    Update HelloWorld.java

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -1,7 +1,11 @@
 package oata;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World")
+    public static int max(int a, int b){
+        if (a > b){
+            return a;
+        }
+        else
+            return b;
     }
 }
